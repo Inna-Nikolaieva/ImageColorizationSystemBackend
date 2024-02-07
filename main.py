a1bbc9ad6b3500c9b390de43fe066c83b6517a2a
Inna Nikolaieva <anastasiiainna@gmail.com>
2024-02-07 23:02:26 +0200

    Added a user router.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from db import models
+from db.database import engine
+from routers import user
 
+app = FastAPI()
+app.include_router(user.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+
+models.Base.metadata.create_all(engine)
+
